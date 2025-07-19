15946f2176f53cc667e663396cc18e7a4a158773
Benie Gouli <gouliaudrey@gmail.com>
2025-07-19 00:25:19 +0000

    I initialized the basics of the game logic. I noticed some flaws in the logic I had set before. Must fix that

--- scripts/game_logic.gd
+++ scripts/game_logic.gd
@@ -1,1 +1,74 @@
 extends Node
+
+## --- storing all the moves 
+var all_moves: Array = [
+	"rock",
+	"paper",
+	"scissors",
+	"fire",
+	"water",
+	"sword",
+	"shield",
+	"corruption"
+]
+
+## --- Relationships between each moves
+var rock_set: Dictionary = {
+	"beat": [],
+	"lose": [],
+	"tie": [],
+}
+
+var paper_set: Dictionary = {
+	"beat": [],
+	"lose": [],
+	"tie": [],
+}
+
+var scissors_set: Dictionary = {
+	"beat": [],
+	"lose": [],
+	"tie": [],
+}
+
+var fire_set: Dictionary = {
+	"beat": [],
+	"lose": [],
+	"tie": [],
+}
+
+var water_set: Dictionary = {
+	"beat": [],
+	"lose": [],
+	"tie": [],
+}
+
+var sword_set: Dictionary = {
+	"beat": [],
+	"lose": [],
+	"tie": [],
+}
+
+var shield_set: Dictionary = {
+	"beat": [],
+	"lose": [],
+	"tie": [],
+}
+
+var corruption_set: Dictionary = {
+	"beat": [],
+	"lose": [],
+	"tie": [],
+}
+
+## --- 
+var all_moves_list: Array = [
+	rock_set,
+	paper_set,
+	scissors_set,
+	fire_set,
+	water_set,
+	sword_set,
+	shield_set,
+	corruption_set,
+]
