5a61444f0e5ae81c5ba58aa94f63cf3f5553af53
Benie Gouli <gouliaudrey@gmail.com>
2025-07-28 01:58:00 +0000

    Added a warning message before the game starts

--- scripts/game_handling.gd
+++ scripts/game_handling.gd
@@ -1,5 +1,7 @@
 extends Node
 
+signal start_game
+
 var move_played_by_p1: String
 var move_played_by_bot: String 
 var move_textures: Dictionary = {
--- timer.gd
+++ timer.gd
@@ -7,9 +7,12 @@ var label
 func _ready() -> void:
 	timer = $Timer
 	label = $Label
+	GameHandling.start_game.connect(start_timer)
+
+
+func start_timer() -> void:
 	timer.start()
 	$AudioStreamPlayer.play()
-
 func update_text() -> void:
 	label.text = str(ceili(timer.time_left))
 	# ceili is used to round the time left to the to the nearest int (upwards)
