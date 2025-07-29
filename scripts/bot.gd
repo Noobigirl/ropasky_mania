1960655cfa5a8ed0473285fabaa273eb56b6a299
Benie Gouli <gouliaudrey@gmail.com>
2025-07-29 21:02:44 +0000

    Revert "Trying to make basic bot functionalities work"
    This reverts commit a0a014f9f567ef525bf33ef6504670c8394163d5.

--- scripts/bot.gd
+++
@@ -1,6 +0,0 @@
-extends Node
-
-func _ready() -> void:
-	#--- in easy mode, bot just chooses random move (for now)
-	var move_to_play = GameLogic.all_moves.pick_random()
-	GameHandling.move_played_by_bot = move_to_play
