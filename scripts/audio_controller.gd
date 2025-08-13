825b67b506397f88a9ab46e1062a285b84b78aaa
Benie Gouli <gouliaudrey@gmail.com>
2025-08-13 01:37:44 +0000

    working on audio management

---
+++ scripts/audio_controller.gd
@@ -0,0 +1,16 @@
+extends Node
+
+
+func play_audio(audio_name: String) -> void:
+	var active_stream = $Main
+	print(active_stream)
+	active_stream.play()
+	
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
